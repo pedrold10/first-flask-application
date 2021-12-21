bbe729c62a27969c2bc157b2bf54c1e71bb74875
Pedro Gabriel <pedrogabriel12423@gmail.com>
2021-12-21 00:22:24 -0300

    Adicionando chamada de API

---
+++ stores.py
@@ -0,0 +1,15 @@
+stores = [
+    {
+        'name': 'Beautiful Store',
+        'items': [
+            {
+                'name': 'Tênis',
+                'price': 299.99
+            },
+            {
+                'name': 'Camisa',
+                'price': 29.99
+            }
+        ]
+    }
+]
